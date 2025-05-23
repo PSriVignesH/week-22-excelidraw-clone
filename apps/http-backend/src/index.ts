2defc15721b5871327096f7ab38d67c99742372b
Sri Vignesh <srivignesh.p@enfec.com>
2025-05-23 16:20:35 +0530

    feat: finsihed chat backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -65,11 +65,13 @@ try {
     return
   }
 
-  const token = jwt.sign( user.id, JWT_SECRET)
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET)
   res.json({
      token
   })
 } catch (e) {
+  console.log(e,"error");
+  
    res.status(401).json({
        message:"Unauthorized"
      })
@@ -107,6 +109,44 @@ app.post("/room", middleware, async (req, res) => {
             message: "Room already exists with this name"
         })
     }
+})
+app.get("/chats/:roomId", async (req, res) => {
+    try {
+        const roomId = Number(req.params.roomId);
+        console.log(req.params.roomId);
+        const messages = await prismaClient.chat.findMany({
+            where: {
+                roomId: roomId
+            },
+            orderBy: {
+                id: "desc"
+            },
+            take: 1000
+        });
+
+        res.json({
+            messages
+        })
+    } catch(e) {
+        console.log(e);
+        res.json({
+            messages: []
+        })
+    }
+    
+})
+
+app.get("/room/:slug", async (req, res) => {
+    const slug = req.params.slug;
+    const room = await prismaClient.room.findFirst({
+        where: {
+            slug
+        }
+    });
+
+    res.json({
+        room
+    })
 })
 
 app.listen(3001)
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,7 @@
 import { WebSocket, WebSocketServer } from 'ws'
 import jwt from 'jsonwebtoken'
 import { JWT_SECRET } from '@repo/backend-common/config'
-
+import {prismaClient} from '@repo/db/client'
 const wss = new WebSocketServer({ port: 8080 })
 
 interface Users{
@@ -53,7 +53,7 @@ wss.on('connection', function connection(ws, request) {
     ws
   })
 
-  ws.on('message', function message(data) {
+  ws.on('message', async function message(data) {
     const parsedData = JSON.parse(data as unknown as string)
     if (parsedData.type === 'join_room') {
       const user = users.find((x) => x.ws === ws)
@@ -71,6 +71,15 @@ wss.on('connection', function connection(ws, request) {
     if (parsedData.type === 'chat') {
       const roomId = parsedData.roomId
       const message = parsedData.message
+
+      await prismaClient.chat.create({
+        data: {
+          roomId,
+          message,
+          userId
+        }
+      })
+      
       users.forEach((user) => {
         if (user.rooms.includes(roomId)) {
           user.ws.send(JSON.stringify({
@@ -81,6 +90,5 @@ wss.on('connection', function connection(ws, request) {
         }
       })
     }
-    ws.send('pong')
   })
 })
